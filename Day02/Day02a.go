b0eaec93633100a7991b3cc77462b2998438d6f6
David M <1900179+medzernik@users.noreply.github.com>
2022-12-02 11:08:01 +0100

    Changes by a piece of shit

--- Day02/Day02a.go
+++ Day02/Day02a.go
@@ -9,5 +9,69 @@ import (
 )
 
 func RunDay02a() {
-	println("hello")
+	f, err := os.Open("./Day02/input_filip02.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+	var lineArray []string
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		lineArray = append(lineArray, scanner.Text())
+	}
+
+	var score int
+
+	for i := range lineArray {
+		strategy := strings.Fields(lineArray[i])
+		opponent := strategy[0]
+
+		switch strategy[1] {
+		case "X":
+			score += 1
+
+			switch opponent {
+			case "C":
+				score += 6
+
+			case "A":
+				score += 3
+
+			case "B":
+				score += 0
+			}
+
+		case "Y":
+			score += 2
+
+			switch opponent {
+			case "A":
+				score += 6
+			case "B":
+				score += 3
+			case "C":
+				score += 0
+			}
+
+		case "Z":
+			score += 3
+
+			switch opponent {
+			case "B":
+				score += 6
+			case "C":
+				score += 3
+			case "A":
+				score += 0
+			}
+		}
+
+	}
+	fmt.Printf("Day 02a result is: %d\n", score)
+
 }
--- Day02/Day02b.go
+++ Day02/Day02b.go
@@ -1,5 +1,75 @@
 package Day02
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
 func RunDay02b() {
-	println("Helloo")
+	f, err := os.Open("./Day02/input_filip02.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+	var lineArrays []string
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		lineArrays = append(lineArrays, scanner.Text())
+	}
+
+	var score int
+
+	for i := range lineArrays {
+		strategy := strings.Fields(lineArrays[i])
+		opponent := strategy[0]
+
+		switch strategy[1] {
+		case "X":
+			score += 0
+
+			switch opponent {
+			case "A":
+				score += 3
+			case "B":
+				score += 1
+			case "C":
+				score += 2
+			}
+
+		case "Y":
+			score += 3
+
+			switch opponent {
+			case "A":
+				score += 1
+			case "B":
+				score += 2
+			case "C":
+				score += 3
+			}
+
+		case "Z":
+			score += 6
+
+			switch opponent {
+			case "A":
+				score += 2
+			case "B":
+				score += 3
+			case "C":
+				score += 1
+			}
+		}
+
+	}
+	fmt.Printf("Day 02b result is: %d\n", score)
+
 }
