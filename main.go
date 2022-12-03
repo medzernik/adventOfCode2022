8f405b44d0d1efe54bb07554cbeaace475939143
David M <1900179+medzernik@users.noreply.github.com>
2022-12-03 22:52:28 +0100

    prep for day 3

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"adventOfCode2022/Day02"
+	"adventOfCode2022/Day03"
 )
 
 func main() {
 	//Day01.RunDay01a()
 	//Day01.RunDay01b()
 
-	Day02.RunDay02a()
-	Day02.RunDay02b()
+	//Day02.RunDay02a()
+	//Day02.RunDay02b()
 
+	Day03.RunDay03a()
+	Day03.RunDay03b()
 }
