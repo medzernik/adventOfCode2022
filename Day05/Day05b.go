1c1f5c5facef8df2e8382a07be8e9a45a08e686d
David M <1900179+medzernik@users.noreply.github.com>
2022-12-05 22:49:33 +0100

    Changes by JoshTheSnom

--- Day05/Day05b.go
+++ Day05/Day05b.go
@@ -1,5 +1,57 @@
 package Day05
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
 func RunDay05b() {
+	f, err := os.Open("./Day05/input_ja.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	stacks := make([][]rune, 9)
+
+	for scanner.Text() != " 1   2   3   4   5   6   7   8   9" { //there should be an extra space after 9, but you can try what happens if you add it yourself :))))
+		for i, sym := range scanner.Text() {
+			if sym != ' ' && sym != '[' && sym != ']' {
+				stacks[(i-1)/4] = append([]rune{sym}, stacks[(i-1)/4]...)
+			}
+		}
+		fmt.Println(stacks)
+		scanner.Scan()
+	}
+
+	scanner.Scan()
+	for scanner.Scan() {
+		var amount, from, to int
+
+		fmt.Sscanf(scanner.Text(), "move %d from %d to %d", &amount, &from, &to)
+
+		//move to box
+		//for i := 0; i < amount; i++ {
+		//there should be proper stack implementation methods here
+
+		ti, fi, l := to-1, from-1, len(stacks[from-1])             //"ti" and "fi" stand for "to index" and "from index"
+		stacks[ti] = append(stacks[ti], stacks[fi][l-amount:l]...) //adds amount of items from "from" stack to "to" stack
+		stacks[fi] = stacks[fi][:l-amount]                         //removes amount of items from "from" stack
+
+		//fmt.Println(stacks[fi])
+		//}
+
+		fmt.Println(amount, from, to)
+
+	}
+
+	for _, box := range stacks {
+		fmt.Print(string(box[len(box)-1])) //prints last of each stack
+	}
 
 }
