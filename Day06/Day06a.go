05acd810ad75d31677b6420c0b4dda0dceeabb16
David M <1900179+medzernik@users.noreply.github.com>
2022-12-06 19:19:15 +0100

    Day 06 done

--- Day06/Day06a.go
+++ Day06/Day06a.go
@@ -7,7 +7,7 @@ import (
 )
 
 func RunDay06a() {
-	f, err := os.Open("./Day03/input_ja.txt")
+	f, err := os.Open("./Day06/input_filip.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -18,7 +18,30 @@ func RunDay06a() {
 
 	scanner := bufio.NewScanner(f)
 
-	for scanner.Scan() {
-		lineArray = append(lineArray, scanner.Text())
+	scanner.Scan()
+
+	for i := range scanner.Text() {
+		var characters []int
+
+		for j := 0; j < 4; j++ {
+			if contains(characters, int(scanner.Text()[i+j])) == false {
+				characters = append(characters, int(scanner.Text()[i+j])) //if characters array doesn't already contain a character, add it
+			}
+		}
+
+		if len(characters) == 4 {
+			fmt.Println(i + 4)
+			break
+		}
 	}
+}
+
+func contains(s []int, i int) bool {
+	for _, v := range s {
+		if v == i {
+			return true
+		}
+	}
+
+	return false
 }
